1288950c17af777a643b4d2e4489d6772faf443a
Ubuntu <grdvincent2@gmail.com>
2020-12-11 10:56:59 +0000

    ajout dump BDD & README

--- database/CCIAPPRO.sql
+++ database/CCIAPPRO.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : ven. 27 nov. 2020 à 14:20
+-- Généré le : ven. 11 déc. 2020 à 10:53
 -- Version du serveur :  8.0.22-0ubuntu0.20.10.2
 -- Version de PHP : 7.4.9
 
@@ -31,13 +31,13 @@ SET time_zone = "+00:00";
 CREATE TABLE `commandes` (
   `id` bigint UNSIGNED NOT NULL,
   `idCommande` int NOT NULL,
-  `quantite` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `quantite` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `dateCommande` date NOT NULL,
   `idProduit` bigint UNSIGNED NOT NULL,
   `idEtat` bigint UNSIGNED NOT NULL,
   `idUser` bigint UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -45,62 +45,22 @@ CREATE TABLE `commandes` (
 --
 
 INSERT INTO `commandes` (`id`, `idCommande`, `quantite`, `dateCommande`, `idProduit`, `idEtat`, `idUser`, `created_at`, `updated_at`) VALUES
-(1, 1, '1', '2020-11-09', 1, 1, 1, NULL, NULL),
-(2, 1, '2', '2020-11-09', 2, 1, 1, NULL, NULL),
-(3, 1, '3', '2020-11-09', 1, 3, 2, NULL, NULL),
-(4, 2, '1', '2020-11-11', 1, 1, 1, '2020-11-11 15:02:47', '2020-11-11 15:02:47'),
-(5, 2, '1', '2020-11-11', 2, 1, 1, '2020-11-11 15:02:47', '2020-11-11 15:02:47'),
-(6, 2, '1', '2020-11-11', 3, 1, 1, '2020-11-11 15:02:48', '2020-11-11 15:02:48'),
-(7, 2, '1', '2020-11-11', 4, 1, 1, '2020-11-11 15:02:48', '2020-11-11 15:02:48'),
-(8, 2, '1', '2020-11-11', 5, 1, 1, '2020-11-11 15:02:49', '2020-11-11 15:02:49'),
-(9, 2, '1', '2020-11-11', 6, 1, 1, '2020-11-11 15:02:49', '2020-11-11 15:02:49'),
-(10, 2, '1', '2020-11-11', 7, 1, 1, '2020-11-11 15:02:49', '2020-11-11 15:02:49'),
-(11, 3, '1', '2020-11-11', 2, 1, 2, '2020-11-11 15:05:48', '2020-11-11 15:05:48'),
-(12, 3, '1', '2020-11-11', 3, 1, 2, '2020-11-11 15:05:48', '2020-11-11 15:05:48'),
-(13, 4, '1', '2020-11-11', 4, 1, 1, '2020-11-11 15:55:44', '2020-11-11 15:55:44'),
-(14, 4, '1', '2020-11-11', 7, 1, 1, '2020-11-11 15:55:44', '2020-11-11 15:55:44'),
-(15, 5, '1', '2020-11-11', 4, 1, 1, '2020-11-11 15:56:42', '2020-11-11 15:56:42'),
-(16, 5, '1', '2020-11-11', 7, 1, 1, '2020-11-11 15:56:42', '2020-11-11 15:56:42'),
-(17, 6, '1', '2020-11-11', 4, 1, 1, '2020-11-11 15:57:02', '2020-11-11 15:57:02'),
-(18, 6, '1', '2020-11-11', 7, 1, 1, '2020-11-11 15:57:03', '2020-11-11 15:57:03'),
-(19, 7, '1', '2020-11-11', 4, 1, 1, '2020-11-11 15:58:10', '2020-11-11 15:58:10'),
-(20, 7, '1', '2020-11-11', 7, 1, 1, '2020-11-11 15:58:11', '2020-11-11 15:58:11'),
-(21, 8, '1', '2020-11-11', 4, 1, 1, '2020-11-11 15:59:15', '2020-11-11 15:59:15'),
-(22, 8, '1', '2020-11-11', 7, 1, 1, '2020-11-11 15:59:15', '2020-11-11 15:59:15'),
-(23, 9, '1', '2020-11-11', 2, 1, 1, '2020-11-11 16:04:36', '2020-11-11 16:04:36'),
-(24, 10, '1', '2020-11-11', 1, 1, 1, '2020-11-11 16:05:45', '2020-11-11 16:05:45'),
-(25, 11, '1', '2020-11-11', 3, 1, 1, '2020-11-11 16:07:07', '2020-11-11 16:07:07'),
-(26, 12, '1', '2020-11-11', 5, 1, 1, '2020-11-11 16:08:17', '2020-11-11 16:08:17'),
-(27, 13, '1', '2020-11-11', 1, 1, 1, '2020-11-11 16:15:20', '2020-11-11 16:15:20'),
-(28, 13, '1', '2020-11-11', 2, 1, 1, '2020-11-11 16:15:20', '2020-11-11 16:15:20'),
-(29, 13, '1', '2020-11-11', 3, 1, 1, '2020-11-11 16:15:21', '2020-11-11 16:15:21'),
-(30, 13, '2', '2020-11-11', 4, 1, 1, '2020-11-11 16:15:21', '2020-11-11 16:15:21'),
-(31, 13, '1', '2020-11-11', 5, 1, 1, '2020-11-11 16:15:22', '2020-11-11 16:15:22'),
-(32, 13, '1', '2020-11-11', 6, 1, 1, '2020-11-11 16:15:22', '2020-11-11 16:15:22'),
-(33, 13, '5', '2020-11-11', 7, 1, 1, '2020-11-11 16:15:22', '2020-11-11 16:15:22'),
-(34, 14, '1', '2020-11-11', 3, 1, 3, '2020-11-11 16:18:03', '2020-11-11 16:18:03'),
-(35, 14, '1', '2020-11-11', 2, 1, 3, '2020-11-11 16:18:04', '2020-11-11 16:18:04'),
-(36, 14, '1', '2020-11-11', 5, 1, 3, '2020-11-11 16:18:04', '2020-11-11 16:18:04'),
-(37, 15, '1', '2020-11-11', 7, 1, 2, '2020-11-11 16:19:22', '2020-11-11 16:19:22'),
-(38, 15, '1', '2020-11-11', 4, 1, 2, '2020-11-11 16:19:22', '2020-11-11 16:19:22'),
-(39, 16, '1', '2020-11-11', 2, 1, 1, '2020-11-11 17:50:46', '2020-11-11 17:50:46'),
-(40, 16, '1', '2020-11-11', 3, 1, 1, '2020-11-11 17:50:46', '2020-11-11 17:50:46'),
-(41, 17, '3', '2020-11-27', 2, 1, 1, '2020-11-27 09:50:26', '2020-11-27 09:50:26'),
-(42, 17, '2', '2020-11-27', 1, 1, 1, '2020-11-27 09:50:27', '2020-11-27 09:50:27'),
-(43, 18, '1', '2020-11-27', 2, 1, 7, '2020-11-27 12:09:19', '2020-11-27 12:09:19'),
-(44, 18, '1', '2020-11-27', 1, 1, 7, '2020-11-27 12:09:20', '2020-11-27 12:09:20'),
-(45, 19, '1', '2020-11-27', 6, 1, 7, '2020-11-27 12:09:29', '2020-11-27 12:09:29'),
-(46, 19, '1', '2020-11-27', 7, 1, 7, '2020-11-27 12:09:30', '2020-11-27 12:09:30'),
-(47, 20, '1', '2020-11-27', 5, 1, 7, '2020-11-27 12:24:11', '2020-11-27 12:24:11'),
-(48, 20, '1', '2020-11-27', 2, 1, 7, '2020-11-27 12:24:12', '2020-11-27 12:24:12'),
-(49, 20, '1', '2020-11-27', 3, 3, 7, '2020-11-27 12:24:12', '2020-11-27 12:24:12'),
-(50, 21, '1', '2020-11-27', 1, 4, 7, '2020-11-27 12:38:56', '2020-11-27 12:38:56'),
-(51, 21, '1', '2020-11-27', 2, 1, 7, '2020-11-27 12:38:56', '2020-11-27 12:38:56'),
-(52, 21, '1', '2020-11-27', 3, 1, 7, '2020-11-27 12:38:56', '2020-11-27 12:38:56'),
-(53, 21, '1', '2020-11-27', 4, 1, 7, '2020-11-27 12:38:56', '2020-11-27 12:38:56'),
-(54, 21, '1', '2020-11-27', 5, 1, 7, '2020-11-27 12:38:57', '2020-11-27 12:38:57'),
-(55, 21, '1', '2020-11-27', 6, 1, 7, '2020-11-27 12:38:58', '2020-11-27 12:38:58'),
-(56, 21, '2', '2020-11-27', 7, 1, 7, '2020-11-27 12:38:58', '2020-11-27 12:38:58');
+(57, 1, '1', '2020-12-06', 1, 3, 1, '2020-12-06 14:22:53', '2020-12-06 15:24:52'),
+(58, 1, '1', '2020-12-06', 7, 3, 1, '2020-12-06 14:22:54', '2020-12-06 15:24:52'),
+(59, 2, '1', '2020-12-06', 2, 3, 1, '2020-12-06 15:06:50', '2020-12-06 15:25:46'),
+(60, 2, '1', '2020-12-06', 3, 3, 1, '2020-12-06 15:06:50', '2020-12-06 15:26:06'),
+(61, 3, '1', '2020-12-06', 5, 3, 1, '2020-12-06 15:07:02', '2020-12-06 15:26:17'),
+(62, 3, '1', '2020-12-06', 6, 3, 1, '2020-12-06 15:07:03', '2020-12-07 21:06:46'),
+(63, 4, '1', '2020-12-06', 4, 3, 1, '2020-12-06 15:08:42', '2020-12-06 15:26:12'),
+(64, 4, '1', '2020-12-06', 1, 2, 1, '2020-12-06 15:08:42', '2020-12-06 15:09:02'),
+(65, 5, '1', '2020-12-06', 2, 4, 1, '2020-12-06 15:26:27', '2020-12-06 15:28:27'),
+(66, 6, '1', '2020-12-07', 1, 2, 1, '2020-12-07 13:17:51', '2020-12-07 13:22:15'),
+(67, 7, '1', '2020-12-07', 3, 2, 1, '2020-12-07 13:23:27', '2020-12-07 13:24:30'),
+(68, 8, '1', '2020-12-07', 5, 2, 1, '2020-12-07 13:25:04', '2020-12-07 13:25:08'),
+(69, 9, '1', '2020-12-07', 4, 2, 1, '2020-12-07 13:26:51', '2020-12-07 13:28:17'),
+(70, 10, '1', '2020-12-07', 2, 2, 1, '2020-12-07 13:29:26', '2020-12-07 13:31:50'),
+(71, 11, '1', '2020-12-07', 7, 4, 1, '2020-12-07 13:41:02', '2020-12-07 13:41:13'),
+(72, 11, '1', '2020-12-07', 4, 4, 1, '2020-12-07 13:41:02', '2020-12-07 13:41:13');
 
 -- --------------------------------------------------------
 
@@ -110,7 +70,7 @@ INSERT INTO `commandes` (`id`, `idCommande`, `quantite`, `dateCommande`, `idProd
 
 CREATE TABLE `etats` (
   `idEtat` bigint UNSIGNED NOT NULL,
-  `etat` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `etat` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -133,10 +93,10 @@ INSERT INTO `etats` (`idEtat`, `etat`, `created_at`, `updated_at`) VALUES
 
 CREATE TABLE `failed_jobs` (
   `id` bigint UNSIGNED NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -148,7 +108,7 @@ CREATE TABLE `failed_jobs` (
 
 CREATE TABLE `marque_produits` (
   `idMarqueProduit` bigint UNSIGNED NOT NULL,
-  `marqueProduit` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `marqueProduit` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -163,7 +123,29 @@ INSERT INTO `marque_produits` (`idMarqueProduit`, `marqueProduit`, `created_at`,
 (3, 'BIC', NULL, NULL),
 (4, 'STABILO', NULL, NULL),
 (5, 'SCOTCH', NULL, NULL),
-(6, 'OXFORD', NULL, NULL);
+(6, 'OXFORD', NULL, NULL),
+(7, 'Commande spécifique', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` bigint UNSIGNED NOT NULL,
+  `message` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `isActive` tinyint(1) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `messages`
+--
+
+INSERT INTO `messages` (`id`, `message`, `isActive`, `created_at`, `updated_at`) VALUES
+(1, 'Bonjour et bienvenue sur CCIAPPRO !', 1, '2020-12-11 10:51:22', '2020-12-11 10:51:22');
 
 -- --------------------------------------------------------
 
@@ -173,7 +155,7 @@ INSERT INTO `marque_produits` (`idMarqueProduit`, `marqueProduit`, `created_at`,
 
 CREATE TABLE `migrations` (
   `id` int UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `migration` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -190,7 +172,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (6, '2020_11_07_191911_create_type_produits_table', 3),
 (7, '2020_11_07_192020_create_produits_table', 4),
 (8, '2020_11_09_133709_create_etats_table', 5),
-(9, '2020_11_09_134415_create_commandes_table', 5);
+(9, '2020_11_09_134415_create_commandes_table', 5),
+(10, '2020_12_07_211043_add_is_activ_produit', 6),
+(11, '2020_12_11_090128_create_messages_table', 7);
 
 -- --------------------------------------------------------
 
@@ -199,8 +183,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 --
 
 CREATE TABLE `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -212,28 +196,30 @@ CREATE TABLE `password_resets` (
 
 CREATE TABLE `produits` (
   `idProduit` bigint UNSIGNED NOT NULL,
-  `nomProduit` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `descProduit` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nomProduit` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descProduit` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `stockProduit` int NOT NULL,
-  `imgProduit` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `imgProduit` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `idMarqueProduit` bigint UNSIGNED NOT NULL,
   `idTypeProduit` bigint UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `isActive` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `produits`
 --
 
-INSERT INTO `produits` (`idProduit`, `nomProduit`, `descProduit`, `stockProduit`, `imgProduit`, `idMarqueProduit`, `idTypeProduit`, `created_at`, `updated_at`) VALUES
-(1, 'Baton de colle', 'Baton de colle pour papier', 10, './img/colle.png', 1, 1, NULL, NULL),
-(2, 'Ciseaux', 'Ciseaux pour découpe papier', 10, './img/ciseaux.png', 2, 2, NULL, NULL),
-(3, 'Ruban adhésif', 'Ruban ashésif fixation forte', 10, './img/scotch.png', 5, 5, NULL, NULL),
-(4, 'Colle liquide', 'Colle liquide fixation forte', 10, './img/colle2.png', 5, 1, NULL, NULL),
-(5, 'Règle', 'Règle de 30cm', 10, './img/regle.png', 2, 3, NULL, NULL),
-(6, 'Surligneurs', 'Surligneurs de couleur', 10, './img/fluo.png', 4, 4, NULL, NULL),
-(7, 'Cahier', 'Cahiers grands carreaux', 10, './img/cahier.png', 6, 6, NULL, NULL);
+INSERT INTO `produits` (`idProduit`, `nomProduit`, `descProduit`, `stockProduit`, `imgProduit`, `idMarqueProduit`, `idTypeProduit`, `created_at`, `updated_at`, `isActive`) VALUES
+(1, 'Baton de colle', 'Baton de colle pour papier', 10, './storage/shopimg/EdUWgDUbhCXWcJxSyJ7B3YivyyYKMXQwAbfsVYJn.png', 1, 1, NULL, '2020-12-11 07:59:20', 1),
+(2, 'Ciseaux', 'Ciseaux pour découpe papier', 10, './img/ciseaux.png', 2, 2, NULL, '2020-12-07 21:25:15', 1),
+(3, 'Ruban adhésif', 'Ruban ashésif fixation forte', 10, './img/scotch.png', 5, 5, NULL, '2020-12-06 16:17:35', 1),
+(4, 'Colle liquide', 'Colle liquide fixation forte', 10, './img/colle2.png', 5, 1, NULL, '2020-12-06 16:17:37', 1),
+(5, 'Règle', 'Règle de 30cm', 10, './img/regle.png', 2, 3, NULL, '2020-12-06 16:17:40', 1),
+(6, 'Surligneurs', 'Surligneurs de couleur', 9, './img/fluo.png', 4, 4, NULL, '2020-12-07 21:06:46', 1),
+(7, 'Cahier', 'Cahiers grands carreaux', 10, './img/cahier.png', 6, 6, NULL, NULL, 1),
+(8, 'Commande spécifique', 'Pour commander un ou plusieurs articles non disponibles sur CCIAPPRO, veuillez contacter l\'administrateur depuis l\'interface de contact.', 1, './img/special.png', 7, 7, NULL, NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -243,7 +229,7 @@ INSERT INTO `produits` (`idProduit`, `nomProduit`, `descProduit`, `stockProduit`
 
 CREATE TABLE `type_produits` (
   `idTypeProduit` bigint UNSIGNED NOT NULL,
-  `typeProduit` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `typeProduit` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -258,7 +244,8 @@ INSERT INTO `type_produits` (`idTypeProduit`, `typeProduit`, `created_at`, `upda
 (3, 'regle', NULL, NULL),
 (4, 'surligneur', NULL, NULL),
 (5, 'ruban adhesif', NULL, NULL),
-(6, 'cahier', NULL, NULL);
+(6, 'cahier', NULL, NULL),
+(7, 'Commande spécifique', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -274,7 +261,7 @@ CREATE TABLE `users` (
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `dptUser` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `loginUser` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `isAdmin` tinyint(1) DEFAULT NULL,
+  `isAdmin` tinyint(1) NOT NULL DEFAULT '0',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -287,8 +274,8 @@ INSERT INTO `users` (`id`, `name`, `firstname`, `email`, `password`, `dptUser`, 
 (1, 'Grande', 'Vincent', 'vincent.grande@outlook.fr', '$2y$10$XAo9wgpsCinivOW2eoRSDOZ6cd9ldxtuuVH8wuAJ/sIpAhDf/Tj3q', 'comptabilite', 'VGRANDE', 1, '2020-11-07 19:38:17', '2020-11-26 21:00:17'),
 (2, 'Haller', 'Antoine', 'antoine-haller@outlook.fr', '$2y$10$VQG01znDx6t0ud2lgQXzDuulEiuQVLgxPNVstNarZRyGLRS./T9bC', 'accueil', 'AHALLER', 0, '2020-11-07 19:44:20', '2020-11-27 08:21:30'),
 (3, 'Golder', 'Lucas', 'cdf.golder.lucas@gmail.com', '$2y$10$Ed7tqIba8LHEsGpI1BLkOuttUG5xTgC1miI4/jLYHnNP0QkScweBS', 'comptabilite', 'LGOLDER', 0, '2020-11-11 16:17:33', '2020-11-11 16:17:33'),
-(7, 'Test', 'Test', 'test@test.fr', '$2y$10$sRkQNRNHanrUC.xMCXKcdO13aXBZ6Qby0F.S6MS9IntmSjG0o2fOW', 'accueil', 'TTEST', NULL, '2020-11-27 12:09:03', '2020-11-27 12:09:03'),
-(8, 'Grande', 'Vincent', 'vincent.grande@free.fr', '$2y$10$2Ig4D8sp9M1nQ.iKhuE86uoevOD0Ifql3K9DCusBIu1aVQtdIAE5W', 'comptabilite', 'VGRANDE2', NULL, '2020-11-27 13:38:14', '2020-11-27 13:38:14');
+(7, 'Test', 'Test', 'test@test.fr', '$2y$10$sRkQNRNHanrUC.xMCXKcdO13aXBZ6Qby0F.S6MS9IntmSjG0o2fOW', 'accueil', 'TTEST', 0, '2020-11-27 12:09:03', '2020-11-27 12:09:03'),
+(8, 'Grande', 'Vincent', 'vincent.grande@free.fr', '$2y$10$2Ig4D8sp9M1nQ.iKhuE86uoevOD0Ifql3K9DCusBIu1aVQtdIAE5W', 'comptabilite', 'VGRANDE2', 0, '2020-11-27 13:38:14', '2020-11-27 13:38:14');
 
 --
 -- Index pour les tables déchargées
@@ -320,6 +307,12 @@ ALTER TABLE `failed_jobs`
 --
 ALTER TABLE `marque_produits`
   ADD PRIMARY KEY (`idMarqueProduit`);
+
+--
+-- Index pour la table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `migrations`
@@ -362,7 +355,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `commandes`
 --
 ALTER TABLE `commandes`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
 
 --
 -- AUTO_INCREMENT pour la table `etats`
@@ -380,25 +373,31 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT pour la table `marque_produits`
 --
 ALTER TABLE `marque_produits`
-  MODIFY `idMarqueProduit` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `idMarqueProduit` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT pour la table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT pour la table `produits`
 --
 ALTER TABLE `produits`
-  MODIFY `idProduit` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `idProduit` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT pour la table `type_produits`
 --
 ALTER TABLE `type_produits`
-  MODIFY `idTypeProduit` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `idTypeProduit` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT pour la table `users`
